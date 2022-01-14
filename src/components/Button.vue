224145898332cb30f6a35d21a8ba234ea0041a8f
FahmiAndriana <94207375+FahmiAndriana@users.noreply.github.com>
2022-01-15 01:13:00 +0700

    Add files via upload

---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<tamplate>
+<button type="button" class="btn btn-danger">Danger</button>
+</tamplate>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,72 @@
+<template>
+
+<div>
+ <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color:#161E54">
+        <div class="container">
+          <a class="navbar-brand" href="#">Fahmi Andriana</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Project</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- end Navbar -->
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="pp.jpg" width="200"
+        class="rounded-circle img-thumbnail"/>
+        <h1 class="display-4">Fahmi Andriana</h1>
+        <p class="lead">Fullstack Developer</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#161e54" fill-opacity="0.7" d="M0,192L40,176C80,160,160,128,240,133.3C320,139,400,181,480,181.3C560,181,640,139,720,112C800,85,880,75,960,85.3C1040,96,1120,128,1200,149.3C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+      </section>
+    <!-- End Jumbotron -->
+    <!-- About -->
+    <section id="about">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>About Me</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, placeat natus illo vero et tempora ipsa a alias deleniti temporibus?</p>
+                </div>
+                <div class="col-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore id vero porro ex libero facere perspiciatis dicta architecto velit numquam.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About -->
+    
+
+</div>
+   
+</template>
+<script>
+
+</script>
+<style>
+.jumbotron {
+  padding-top: 2rem;
+  background-color: white;
+}
+</style>
